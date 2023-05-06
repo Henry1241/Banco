8c4e3c7848136792336629d3a9be1218def37f88
Henry1241 <enri09ley@outlook.com>
2023-05-06 00:48:27 -0700

    Entidad
    Parámetros de cliente y la clase Tarjeta añadidios

--- Banco/src/mx/itson/banco/entidades/Cliente.java
+++ Banco/src/mx/itson/banco/entidades/Cliente.java
@@ -5,10 +5,73 @@
  */
 package mx.itson.banco.entidades;
 
+import java.util.Date;
+
 /**
  *
- * @author enri0
+ * @author Enrique Gonzalez Leyva
  */
 public class Cliente {
+    
+    private int id;
+    private String nombre;
+    private Date fechaNacimiento;
+    private String correo;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the fechaNacimiento
+     */
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    /**
+     * @param fechaNacimiento the fechaNacimiento to set
+     */
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
     
 }
---
+++ Banco/src/mx/itson/banco/entidades/Tarjeta.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.banco.entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author enri0
+ */
+public class Tarjeta {
+    
+    private int id;
+    private String numeroTarjeta;
+    private int cvv;
+    private Date fechaVencimiento;
+    private double interes;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the numeroTarjeta
+     */
+    public String getNumeroTarjeta() {
+        return numeroTarjeta;
+    }
+
+    /**
+     * @param numeroTarjeta the numeroTarjeta to set
+     */
+    public void setNumeroTarjeta(String numeroTarjeta) {
+        this.numeroTarjeta = numeroTarjeta;
+    }
+
+    /**
+     * @return the cvv
+     */
+    public int getCvv() {
+        return cvv;
+    }
+
+    /**
+     * @param cvv the cvv to set
+     */
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+
+    /**
+     * @return the fechaVencimiento
+     */
+    public Date getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    /**
+     * @param fechaVencimiento the fechaVencimiento to set
+     */
+    public void setFechaVencimiento(Date fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    /**
+     * @return the interes
+     */
+    public double getInteres() {
+        return interes;
+    }
+
+    /**
+     * @param interes the interes to set
+     */
+    public void setInteres(double interes) {
+        this.interes = interes;
+    }
+    
+}
