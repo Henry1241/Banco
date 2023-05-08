d592e1cb2b231f3a8d768f6a4b16db5c90961d3e
Henry1241 <enri09ley@outlook.com>
2023-05-08 10:56:44 -0700

    Conexion
    Creación de la conexión a la base de datos en MYSQL

--- Banco/src/mx/itson/banco/persistencias/Conexion.java
+++ Banco/src/mx/itson/banco/persistencias/Conexion.java
@@ -5,10 +5,26 @@
  */
 package mx.itson.banco.persistencias;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author enri0
  */
 public class Conexion {
-    
+ 
+    /**
+     * Obtiene una conexión hacia la base de datos utilizando los parámetros proporcionados.
+     * @return La conexión inicializada hacia la base de datos.
+     */
+    public static Connection obtener() {
+        Connection conexion = null;
+        try {
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3308/bancodb?user=root&password=Admin4422");
+        } catch (Exception ex){
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        }
+        return conexion;
+    }
 }
